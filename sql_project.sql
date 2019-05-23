00d7bb6b1d83aaf94b8c0431e5f9b846cd920aaa
georgecctang <48698540+georgecctang@users.noreply.github.com>
2019-05-22 23:29:44 -0400

    Update 1 (incomplete)

--- sql_project.sql
+++ sql_project.sql
@@ -27,8 +27,15 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
 
+SELECT * FROM Facilities
+where membercost > 0;
+
 
 /* Q2: How many facilities do not charge a fee to members? */
+
+SELECT COUNT( * ) 
+FROM Facilities
+WHERE membercost = 0;
 
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
@@ -36,25 +43,56 @@ where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost >0
+AND membercost < monthlymaintenance * 0.2;
+
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
 
+SELECT * 
+FROM Facilities
+WHERE facid
+IN ( 1, 5 );
 
 /* Q5: How can you produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100? Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name, 
+CASE WHEN monthlymaintenance >100
+THEN  'expensive'
+ELSE  'cheap'
+END AS monthlymaintenance
+FROM Facilities;
+
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Do not use the LIMIT clause for your solution. */
+
+SELECT firstname, surname
+FROM  `Members` 
+WHERE memid = ( 
+SELECT MAX( memid ) 
+FROM Members );
 
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
+
+SELECT DISTINCT CONCAT( firstname,  " ", surname ) AS  'fullname', f.name AS  'facility'
+FROM Bookings AS b
+INNER JOIN Members AS m ON b.memid = m.memid
+INNER JOIN Facilities AS f ON b.facid = f.facid
+WHERE m.memid !=0
+AND f.facid
+IN ( 0, 1 ) 
+ORDER BY fullname;
 
 
 /* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
@@ -65,9 +103,13 @@ facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
 
+
+
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
 
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
+
+
